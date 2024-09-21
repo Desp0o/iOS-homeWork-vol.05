3053870f8f503f074fdeae82358ee38794f7b56f
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-21 15:41:53 +0400

    finish task 1

---
+++ MyPlayground.playground/Pages/task1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+//1. შექმენით მასივი `fruits`, რომელიც შეიცავს 5 ხილის სახელს. გამოიყენეთ `forEach` მეთოდი, რომ დაბეჭდოთ თითოეული ხილის სახელი ცალ-ცალკე ხაზზე.
+
+var fruits = ["Mango", "Pineapple", "Papaya", "Coconut", "Passionfruit"];
+
+fruits.forEach { fruit in
+    print(fruit)
+}
