e29b3b586eebd314c54e8b537bc68e1f3562b4c1
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-09-22 15:09:03 +0400

    finish task 10

---
+++ MyPlayground.playground/Pages/task10.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+//: [Previous](@previous)
+
+//10. დაწერეთ ფუნქცია `applyOperations`, რომელიც იღებს Int-ების მასივს და closure-ების მასივს, სადაც თითოეული closure იღებს Int-ს და აბრუნებს Int-ს. ფუნქციამ უნდა გამოიყენოს `map` მეთოდი, რომ ყველა ოპერაცია ჩაატაროს ყველა რიცხვზე და დააბრუნოს შედეგების მასივი. გამოიძახეთ ეს ფუნქცია სხვადასხვა რიცხვებისა და ოპერაციების კომბინაციებით.
+
+import Foundation
+
+let sumClosure: (Int) -> Int = { $0 + 2 }
+let quadMultiplication: (Int) -> Int = { $0 * $0 }
+
+func applyOperations(_ numArray: [Int], _ closureArrays: [(Int) -> Int]) -> [Int] {
+    var finalResult: [Int] = []
+    
+    numArray.map { num in
+        closureArrays.map { closure in
+           let res =  closure(num)
+            finalResult.append(res)
+        }
+    }
+    
+    return finalResult
+}
+
+applyOperations([3, -1, 7, 0, 5, -4, 2, 9, 11], [sumClosure, quadMultiplication])
+
+//: [Next](@next)
